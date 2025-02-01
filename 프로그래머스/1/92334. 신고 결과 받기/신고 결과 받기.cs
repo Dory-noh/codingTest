e5dbc7243bfa8a7032704c4983ca3dd46635dd3a
Dori Noh <52809841+Dory-noh@users.noreply.github.com>
2025-02-01 10:38:34 +0900

    [level 1] Title: 신고 결과 받기, Time: 258.55 ms, Memory: 96 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cs
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cs
@@ -1,31 +1,21 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class Solution {
     public int[] solution(string[] id_list, string[] report, int k) {
         int[] answer = new int[id_list.Length];
+        report = report.Distinct().ToArray(); //중복 신고 제거
         
         //신고된 id를 담는 idDict, 
         //신고한 계정 id를 담는 reportDict, Dictionary 선언 및 초기화
-        //문자열-user ID를 key로 하여 값을 입력받음
-        Dictionary<string, string> idDict = new Dictionary<string, string> {};
-        Dictionary<string, int> reportDict = new Dictionary<string, int> {};
-        //키-값 초기값으로 생성
-        foreach(string id in id_list){
-            idDict.Add(id,"");
-            reportDict.Add(id,0);
-        }
-        //중복 신고 제거를 위한 정렬
-        Array.Sort(report);
+        Dictionary<string, string> idDict = id_list.ToDictionary(str => str, _ => "");
+        Dictionary<string, int> reportDict = id_list.ToDictionary(str => str, _ => 0);
         
-        string beforeRept = "";
         foreach(string rep in report){
-            //중복 신고 내용이면 건너뜀
-            if(beforeRept == rep) continue;
             //해당 계정을 신고한 id를 ' '로 연결함
             string[] temp = rep.Split(' ');
             idDict[temp[1]] += (temp[0]+' ');
-            beforeRept = rep;
         }
         
         for(int i = 0; i < idDict.Count; i++){
