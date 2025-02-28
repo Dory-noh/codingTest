1f0f5d49d36e0ae1c11c543d0e9dd261060f8d2c
Dori Noh <52809841+Dory-noh@users.noreply.github.com>
2025-02-28 14:39:17 +0900

    [level 2] Title: 가장 큰 수, Time: 1523.56 ms, Memory: 41.9 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cs
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cs
@@ -5,8 +5,10 @@ public class Solution {
     public string solution(int[] numbers) {
         
         string[] numStr = numbers.Select(x=>x.ToString()).ToArray();
+        //문자열 두 개를 비교해서 순서를 ab / ba로 배치했을 때, b+a가 a+b보다 크면 b를 먼저 배치하고 a를 나중에 배치한다.
         Array.Sort(numStr, (a,b) => string.Compare(b+a, a+b));
-        string answer = string.Join("", numStr);
+        string answer = string.Join("", numStr); 
+        //numbers의 모든 원소가 0이면 answer의 값은 "0"이다.
         answer = answer[0]=='0' ? "0" : answer;
         return answer;
     }
