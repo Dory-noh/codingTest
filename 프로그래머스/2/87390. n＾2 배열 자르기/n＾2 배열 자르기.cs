5f6b53cb6d6c969f082652a903e974d5e6261cb9
Dori Noh <52809841+Dory-noh@users.noreply.github.com>
2025-03-24 19:42:07 +0900

    [level 2] Title: n^2 배열 자르기, Time: 2.87 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int[] solution(int n, long left, long right) {
+        int len = (int)(right - left + 1);
+        int[] answer = new int[len];
+        for(long i = left; i <= right; i++){
+            answer[(int)(i - left)] = (int)Math.Max(i/n,i%n) + 1;
+        } 
+        return answer;
+    }
+}
