2826adaa41f86bd558a987ceb0c0ac228a7a8c18
Dori Noh <52809841+Dory-noh@users.noreply.github.com>
2025-06-30 01:38:16 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 2.09 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.cs
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.cs
@@ -3,27 +3,7 @@ using System;
 public class Solution {
     public int solution(int[] sides) {
         int answer = 0;
-        int max = sides[0] > sides[1] ? (sides[0] > sides[2] ? 0 : 2) : (sides[1] > sides[2] ? 1 : 2);
-        int a = 0,b = 0,c = 0;
-        switch(max){
-            case 0 : 
-                c = sides[0];
-                a = sides[1];
-                b = sides[2];
-                break;
-            case 1 :
-                c = sides[1];
-                a = sides[0];
-                b = sides[2];
-                break;
-            case 2 :
-                c = sides[2];
-                a = sides[0];
-                b = sides[1];
-                break;
-                default :
-                break;
-        }
-        return answer = c < (a+b) ? 1 : 2;
+        Array.Sort(sides);
+        return answer = sides[2] < (sides[0]+sides[1]) ? 1 : 2;
     }
 }
