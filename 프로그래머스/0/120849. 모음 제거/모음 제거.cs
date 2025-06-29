493a889c5bdf388f78a378523f355db432d51bee
Dori Noh <52809841+Dory-noh@users.noreply.github.com>
2025-06-29 23:19:47 +0900

    [level 0] Title: 모음 제거, Time: 0.78 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.cs
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.cs
@@ -3,17 +3,10 @@ using System;
 public class Solution {
     public string solution(string my_string) {
         string answer = "";
-        string vowelArr = "aeiou";
-        foreach(char ch in my_string){
-            bool isVowel = false;
-            foreach(char vowel in vowelArr){
-                if(ch == vowel) {
-                    isVowel = true;
-                    break;
-                }
-            }
-            if(isVowel == false) answer+=ch;
+        string vowels = "aeiou";
+        foreach(char vowel in vowels){
+            my_string = my_string.Replace(vowel.ToString(),"");
         }
-        return answer;
+        return answer = my_string;
     }
 }
