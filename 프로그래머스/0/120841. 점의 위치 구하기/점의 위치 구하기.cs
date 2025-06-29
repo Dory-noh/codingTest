a63aab23b79ba04afbf6def19055b61727405c9d
Dori Noh <52809841+Dory-noh@users.noreply.github.com>
2025-06-30 01:52:43 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.19 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.cs
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.cs
@@ -2,12 +2,7 @@ using System;
 
 public class Solution {
     public int solution(int[] dot) {
-        int answer = 0;
-        int x=dot[0], y=dot[1];
-        if(x > 0 && y >0) answer = 1;
-        else if(x<0 && y > 0) answer = 2;
-        else if (x <0 && y <0) answer = 3;
-        else if( x > 0 && y < 0) answer = 4;
+        int answer = dot[0] > 0 ? (dot[1] > 0 ? 1 : 4) : (dot[1] > 0 ? 2 : 3);
         return answer;
     }
 }
