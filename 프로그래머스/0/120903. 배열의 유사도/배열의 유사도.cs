e4a6b6d85eb8dfedd90f02a7697c21c0e980b1da
Dori Noh <52809841+Dory-noh@users.noreply.github.com>
2025-07-01 16:17:56 +0900

    [level 0] Title: 배열의 유사도, Time: 1.26 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.cs
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.cs
@@ -1,21 +1,8 @@
 using System;
+using System.Linq;
 
 public class Solution {
     public int solution(string[] s1, string[] s2) {
-        int answer = 0;
-        Array.Sort(s1);
-        Array.Sort(s2);
-        foreach(string s1Str in s1){
-            foreach(string s2Str in s2){
-                //s2의 앞 글자가 s1의 앞 글자보다 사전 순에서 같거나 앞 쪽에 같은 경우까지만 비교 
-                if(s1Str[0] < s2Str[0]){
-                    break;
-                }
-                if(String.Compare(s1Str, s2Str) == 0) {
-                    answer++;
-                }
-            }
-        }
-        return answer;
+        return s1.Count(x=>s2.Contains(x));
     }
 }
