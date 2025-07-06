0e1ae5d575034b4724ca9086079a564880515f17
Dori Noh <52809841+Dory-noh@users.noreply.github.com>
2025-07-07 00:00:48 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 0.74 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.cs
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.cs
@@ -2,7 +2,12 @@ using System;
 
 public class Solution {
     public string solution(string myString) {
-        string answer = myString.ToLower();
+        string answer = "";
+        for(int i = 0; i < myString.Length; i++){
+            string ch = myString[i] + "";
+            if(ch[0] >= 'A') ch = ch.ToLower();
+            answer+=ch;
+        }
         return answer;
     }
 }
