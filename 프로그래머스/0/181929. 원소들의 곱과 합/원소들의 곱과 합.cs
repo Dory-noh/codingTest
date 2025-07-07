6cbd377175985c6f7f5ce1c9ebfae29280dfd2f8
Dori Noh <52809841+Dory-noh@users.noreply.github.com>
2025-07-07 23:59:34 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.27 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int sum = 0;
+        int multiply = 1;
+        for(int i = 0; i < num_list.Length; i++){
+            sum+=num_list[i];
+            multiply*=num_list[i];
+        }
+        answer = multiply < sum * sum ? 1 : 0;
+        return answer;
+    }
+}
