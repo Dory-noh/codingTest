78f80c2f269c6e3222f888cdc64664bcd6b228c0
Dori Noh <52809841+Dory-noh@users.noreply.github.com>
2025-07-12 23:53:36 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.25 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int solution(int[] numbers, int n) {
+        int answer = 0;
+        int idx = 0;
+        while(answer <= n){
+            answer += numbers[idx++];
+        }
+        return answer;
+    }
+}
